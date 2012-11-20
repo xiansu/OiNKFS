55316b5d50f110fd96769118ea9c9d7f60e245c7
Xian Su <xian.w.su@gmail.com>
2012-11-20 13:16:56 -0500

    Initial source commit.

---
+++ oinkfs.py
@@ -0,0 +1,318 @@
+#!/usr/bin/env python
+#
+#   This file is derived from an example file xmp.py by:
+#                  2001  Jeff Epler  <jepler@unpythonic.dhs.org>
+#                  2006  Csaba Henk  <csaba.henk@creo.hu>
+#   Adding my name to the list: :)
+#                  2012  Xian Su <xian.w.su@gmail.com>
+#
+#    This program can be distributed under the terms of the GNU GPL.
+#    If you're a fucking weirdo, don't know about the GPL, and want to see it,
+#    you can get a copy at http://www.gnu.org/licenses/gpl.txt
+#
+
+import os, sys
+from errno import *
+from stat import *
+import fcntl
+
+import fuse
+from fuse import Fuse
+import subprocess
+import re
+import ConfigParser
+import time
+
+if not hasattr(fuse, '__version__'):
+    raise RuntimeError, \
+        "your fuse-py doesn't know of fuse.__version__, probably it's too old."
+
+fuse.fuse_python_api = (0, 2)
+
+# We use a custom file class
+fuse.feature_assert('stateful_files', 'has_init')
+
+j = lambda *p: os.path.join(*p)
+
+config = ConfigParser.ConfigParser()
+config_file=j(os.path.expanduser('~'),'.oinkfs')
+if os.path.isfile(config_file):
+    config.read(config_file)
+else:
+    print "config file %s not found" % config_file
+    sys.exit()
+
+OINK_ROOT=config.get("oinkfs", "OINK_ROOT")
+DEL_FILES=config.get("oinkfs", "DEL_FILES")
+
+
+def flag2mode(flags):
+    md = {os.O_RDONLY: 'r', os.O_WRONLY: 'w', os.O_RDWR: 'w+'}
+    m = md[flags & (os.O_RDONLY | os.O_WRONLY | os.O_RDWR)]
+
+    if flags | os.O_APPEND:
+        m = m.replace('w', 'a', 1)
+
+    return m
+
+
+class OiNKFS(Fuse):
+
+    def __init__(self, *args, **kw):
+
+        Fuse.__init__(self, *args, **kw)
+
+        # do stuff to set up your filesystem here, if you want
+        #import thread
+        #thread.start_new_thread(self.mythread, ())
+        self.root = OINK_ROOT
+        self.file_class = self.OiNKFile
+
+#    def mythread(self):
+#
+#        """
+#        The beauty of the FUSE python implementation is that with the python interp
+#        running in foreground, you can have threads
+#        """
+#        print "mythread: started"
+#        while 1:
+#            time.sleep(120)
+#            print "mythread: ticking"
+
+    def getattr(self, path):
+        return os.lstat("." + path)
+
+    def readlink(self, path):
+        return os.readlink("." + path)
+
+    def readdir(self, path, offset):
+        for e in os.listdir("." + path):
+            if e[-6:] != ".rdiff":
+                yield fuse.Direntry(e)
+
+    def unlink(self, path):
+        os.unlink("." + path)
+
+    def rmdir(self, path):
+        os.rmdir("." + path)
+
+    def symlink(self, path, path1):
+        os.symlink(path, "." + path1)
+
+    def rename(self, path, path1):
+        os.rename("." + path, "." + path1)
+
+    def link(self, path, path1):
+        os.link("." + path, "." + path1)
+
+    def chmod(self, path, mode):
+        os.chmod("." + path, mode)
+
+    def chown(self, path, user, group):
+        os.chown("." + path, user, group)
+
+    def truncate(self, path, len):
+        f = open("." + path, "a")
+        f.truncate(len)
+        f.close()
+
+    def mknod(self, path, mode, dev):
+        os.mknod("." + path, mode, dev)
+
+    def mkdir(self, path, mode):
+        os.mkdir("." + path, mode)
+
+    def utime(self, path, times):
+        os.utime("." + path, times)
+
+#    The following utimens method would do the same as the above utime method.
+#    We can't make it better though as the Python stdlib doesn't know of
+#    subsecond preciseness in acces/modify times.
+#  
+#    def utimens(self, path, ts_acc, ts_mod):
+#      os.utime("." + path, (ts_acc.tv_sec, ts_mod.tv_sec))
+
+    def access(self, path, mode):
+        if not os.access("." + path, mode):
+            return -EACCES
+
+#    This is how we could add stub extended attribute handlers...
+#    (We can't have ones which aptly delegate requests to the underlying fs
+#    because Python lacks a standard xattr interface.)
+#
+#    def getxattr(self, path, name, size):
+#        val = name.swapcase() + '@' + path
+#        if size == 0:
+#            # We are asked for size of the value.
+#            return len(val)
+#        return val
+#
+#    def listxattr(self, path, size):
+#        # We use the "user" namespace to please XFS utils
+#        aa = ["user." + a for a in ("foo", "bar")]
+#        if size == 0:
+#            # We are asked for size of the attr list, ie. joint size of attrs
+#            # plus null separators.
+#            return len("".join(aa)) + len(aa)
+#        return aa
+
+    def statfs(self):
+        """
+        Should return an object with statvfs attributes (f_bsize, f_frsize...).
+        Eg., the return value of os.statvfs() is such a thing (since py 2.2).
+        If you are not reusing an existing statvfs object, start with
+        fuse.StatVFS(), and define the attributes.
+
+        To provide usable information (ie., you want sensible df(1)
+        output, you are suggested to specify the following attributes:
+
+            - f_bsize - preferred size of file blocks, in bytes
+            - f_frsize - fundamental size of file blcoks, in bytes
+                [if you have no idea, use the same as blocksize]
+            - f_blocks - total number of blocks in the filesystem
+            - f_bfree - number of free blocks
+            - f_files - total number of file inodes
+            - f_ffree - nunber of free file inodes
+        """
+
+        return os.statvfs(".")
+
+    def fsinit(self):
+        os.chdir(self.root)
+
+    class OiNKFile(object):
+        global DEL_FILES
+        oinkfile_fullpath = ""
+        oinkfile_difffile = ""
+        oinkfile_tempfile = ""
+        oinkfile_tempdirs = ""
+
+
+        def __init__(self, path, flags, *mode):
+            if not os.path.isdir("./tmp"):
+                os.makedirs("./tmp")
+            log = open("./tmp/oinkfs.log",'a')
+            self.oinkfile_fullpath = "." + path
+            self.oinkfile_difffile = "." + path + ".rdiff"
+            if os.path.isfile(self.oinkfile_difffile):
+                self.oinkfile_tempfile = "." + "/tmp" + path
+                self.oinkfile_tempdirs = re.sub('[^/]*$', '', self.oinkfile_tempfile)
+                if not os.path.isdir(self.oinkfile_tempdirs):
+                    os.makedirs(self.oinkfile_tempdirs)
+                if not os.path.isfile(self.oinkfile_tempfile):
+                    log.write("%s creating %s\n" % (time.strftime('%Y/%m/%d %H:%M:%S'), self.oinkfile_tempfile))
+                    patch = subprocess.Popen(['rdiff', 'patch', self.oinkfile_fullpath, self.oinkfile_difffile, self.oinkfile_tempfile], shell=False)
+                    patch.wait()
+                self.file = os.fdopen(os.open(self.oinkfile_tempfile, flags, *mode), flag2mode(flags))
+                self.fd = self.file.fileno()
+            else:
+                self.file = os.fdopen(os.open(self.oinkfile_fullpath, flags, *mode), flag2mode(flags))
+                self.fd = self.file.fileno()
+
+        def read(self, length, offset):
+            self.file.seek(offset)
+            return self.file.read(length)
+
+        def write(self, buf, offset):
+            self.file.seek(offset)
+            self.file.write(buf)
+            return len(buf)
+
+        def release(self, flags):
+            self.file.close()
+            if DEL_FILES == "1":
+                log = open("./tmp/oinkfs.log",'a')
+                log.write("%s deleting %s\n" % (time.strftime('%Y/%m/%d %H:%M:%S'), self.oinkfile_tempfile))
+                if os.path.isfile(self.oinkfile_tempfile):
+                    os.remove(self.oinkfile_tempfile)
+
+        def _fflush(self):
+            if 'w' in self.file.mode or 'a' in self.file.mode:
+                self.file.flush()
+
+        def fsync(self, isfsyncfile):
+            self._fflush()
+            if isfsyncfile and hasattr(os, 'fdatasync'):
+                os.fdatasync(self.fd)
+            else:
+                os.fsync(self.fd)
+
+        def flush(self):
+            self._fflush()
+            os.close(os.dup(self.fd))
+
+        def fgetattr(self):
+            return os.fstat(self.fd)
+
+        def ftruncate(self, len):
+            self.file.truncate(len)
+
+        def lock(self, cmd, owner, **kw):
+            # The code here is much rather just a demonstration of the locking
+            # API than something which actually was seen to be useful.
+
+            # Advisory file locking is pretty messy in Unix, and the Python
+            # interface to this doesn't make it better.
+            # We can't do fcntl(2)/F_GETLK from Python in a platfrom independent
+            # way. The following implementation *might* work under Linux. 
+            #
+            # if cmd == fcntl.F_GETLK:
+            #     import struct
+            # 
+            #     lockdata = struct.pack('hhQQi', kw['l_type'], os.SEEK_SET,
+            #                            kw['l_start'], kw['l_len'], kw['l_pid'])
+            #     ld2 = fcntl.fcntl(self.fd, fcntl.F_GETLK, lockdata)
+            #     flockfields = ('l_type', 'l_whence', 'l_start', 'l_len', 'l_pid')
+            #     uld2 = struct.unpack('hhQQi', ld2)
+            #     res = {}
+            #     for i in xrange(len(uld2)):
+            #          res[flockfields[i]] = uld2[i]
+            #  
+            #     return fuse.Flock(**res)
+
+            # Convert fcntl-ish lock parameters to Python's weird
+            # lockf(3)/flock(2) medley locking API...
+            op = { fcntl.F_UNLCK : fcntl.LOCK_UN,
+                   fcntl.F_RDLCK : fcntl.LOCK_SH,
+                   fcntl.F_WRLCK : fcntl.LOCK_EX }[kw['l_type']]
+            if cmd == fcntl.F_GETLK:
+                return -EOPNOTSUPP
+            elif cmd == fcntl.F_SETLK:
+                if op != fcntl.LOCK_UN:
+                    op |= fcntl.LOCK_NB
+            elif cmd == fcntl.F_SETLKW:
+                pass
+            else:
+                return -EINVAL
+
+            fcntl.lockf(self.fd, op, kw['l_start'], kw['l_len'])
+
+
+
+def main():
+
+    usage = """
+Userspace nullfs-alike: mirror the filesystem tree from some point on.
+
+""" + Fuse.fusage
+
+    server = OiNKFS(version="%prog " + fuse.__version__,
+                 usage=usage)
+
+    server.multithreaded = False
+
+
+    server.parse(values=server, errex=1)
+
+    try:
+        if server.fuse_args.mount_expected():
+            os.chdir(server.root)
+    except OSError:
+        print >> sys.stderr, "can't enter root of underlying filesystem"
+        sys.exit(1)
+
+    server.main()
+
+
+if __name__ == '__main__':
+    main()
---
+++ oinklink.py
@@ -0,0 +1,397 @@
+#!/usr/bin/env python
+#
+#   This file is created by:
+#                  Xian Su <xian.w.su@gmail.com>
+#
+#    This program can be distributed under the terms of the GNU GPL.
+#    If you're a fucking weirdo, don't know about the GPL, and want to see it,
+#    you can get a copy at http://www.gnu.org/licenses/gpl.txt
+#
+
+import sys
+import os
+import io
+import subprocess
+import hashlib
+import re
+import ConfigParser
+import time
+import locale
+locale.setlocale(locale.LC_ALL, '')
+
+j = lambda *p: os.path.join(*p)
+
+config = ConfigParser.ConfigParser()
+config_file=j(os.path.expanduser('~'),'.oinkfs')
+if os.path.isfile(config_file):
+    config.read(config_file)
+else:
+    print "config file %s not found" % config_file
+    sys.exit()
+
+PERM_ROOT=unicode(config.get("oinklink", "PERM_ROOT"))
+ORIG_ROOT=unicode(config.get("oinklink", "ORIG_ROOT"))
+LINK_ROOT=unicode(config.get("oinklink", "LINK_ROOT"))
+
+total_number_of_album_directories = 0
+total_number_of_directories_to_mk = 0
+total_number_of_match_music_files = 0
+total_number_of_total_music_files = 0
+total_number_of_match_other_files = 0
+total_number_of_total_other_files = 0
+total_size_of_match_music_files = 0
+total_size_of_total_music_files = 0
+total_size_of_match_other_files = 0
+total_size_of_total_other_files = 0
+
+
+class Album:
+    album = u""
+    tracker = u""
+    orig_path = u""
+    perm_path = u""
+    link_path = u""
+    need_link = u""
+
+    def __init__(self, album, tracker):
+        self.orig_log_files = []
+        self.orig_rip_times = []
+        self.perm_log_files = []
+        self.perm_rip_times = []
+
+        self.album = album.encode('utf-8')
+        self.tracker = tracker.encode('utf-8')
+
+        self.orig_path = j(ORIG_ROOT, tracker, album)
+        self.perm_path = j(PERM_ROOT, album)
+        self.link_path = j(LINK_ROOT, tracker, album)
+
+        if os.path.isdir(self.perm_path) and not os.path.isdir(self.link_path):
+            for root, dirs, files in os.walk(self.orig_path):
+                for file in files:
+                    if file[-4:].lower() == ".log":
+                        self.orig_log_files.append(j(root,file).encode('utf-8'))
+
+            for log in self.orig_log_files:
+                c = len(self.orig_rip_times)
+                for line in open(log,'r'):
+                    if "extraction logfile" in line:
+                        self.orig_rip_times.append(line)
+                if c == len(self.orig_rip_times):
+                    for line in io.open(log,'r',encoding='utf-16'):
+                        if "extraction logfile" in line:
+                            self.orig_rip_times.append(line)
+                if c == len(self.orig_rip_times):
+                    print "error reading riptime from %s" % log
+
+            for root, dirs, files in os.walk(self.perm_path):
+                for file in files:
+                    if file[-4:].lower() == ".log":
+                        self.perm_log_files.append(j(root,file).encode('utf-8'))
+
+            for log in self.perm_log_files:
+                c = len(self.perm_rip_times)
+                for line in open(log,'r'):
+                    if "extraction logfile" in line:
+                        self.perm_rip_times.append(line)
+                if c == len(self.perm_rip_times):
+                    for line in io.open(log,'r',encoding='utf-16'):
+                        if "extraction logfile" in line:
+                            self.perm_rip_times.append(line)
+                if c == len(self.perm_rip_times):
+                    print "error reading riptime from %s" % log
+
+            if sorted(set(self.orig_rip_times)) == sorted(set(self.perm_rip_times)):
+                self.need_link = True
+#           sys.stdout.write('+')
+#       else:
+#           sys.stdout.write('.')
+
+
+    def __unicode__(self):
+        return "%s" % (self.orig_path)
+
+    def __str__(self):
+        return unicode(self).encode('utf-8')
+
+    def __eq__(self, b):
+        return self.orig_path == b.orig_path
+
+    def __cmp__(self, b):
+        if self.orig_path == b.orig_path: return 0
+        if self.orig_path < b.orig_path: return -1
+        if self.orig_path > b.orig_path: return 1
+
+    def __hash__(self):
+        return hash(self.orig_path)
+
+    def walk_album(self):
+        self.orig_dirs = []
+        self.orig_files = []
+        self.perm_dirs = []
+        self.perm_files = []
+        self.link_dirs = []
+#       self.link_files = dict()
+
+        for root, dirs, files in os.walk(self.orig_path):
+            for dir in dirs:
+                self.orig_dirs.append(j(root,dir).encode('utf-8'))
+            for file in files:
+                self.orig_files.append(Track(j(root,file).encode('utf-8'), file))
+        for root, dirs, files in os.walk(self.perm_path):
+            for dir in dirs:
+                self.perm_dirs.append(j(root,dir).encode('utf-8'))
+            for file in files:
+                self.perm_files.append(Track(j(root,file).encode('utf-8'), file))
+
+        self.orig_dirs.sort()
+        self.orig_files.sort()
+        self.perm_dirs.sort()
+        self.perm_files.sort()
+        return True
+
+    def pick_track(self):
+        def binary_bytes(bytes):
+            if bytes > 1073741823:
+                return str("%.2f" % (float(bytes)/1073741824)) + "GB"
+            elif bytes > 1048575:
+                return str("%.2f" % (float(bytes)/1048576)) + "MB"
+            elif bytes > 1023:
+                return str("%.2f" % (float(bytes)/1024)) + "KB"
+            else:
+                return str(bytes) + "B"
+            
+        global total_number_of_album_directories
+        global total_number_of_directories_to_mk
+        global total_number_of_match_music_files
+        global total_number_of_total_music_files
+        global total_number_of_match_other_files
+        global total_number_of_total_other_files
+        global total_size_of_match_music_files
+        global total_size_of_total_music_files
+        global total_size_of_match_other_files
+        global total_size_of_total_other_files
+
+        album_number_of_album_directories = 0
+        album_number_of_directories_to_mk = 0
+        album_number_of_match_music_files = 0
+        album_number_of_total_music_files = 0
+        album_number_of_match_other_files = 0
+        album_number_of_total_other_files = 0
+        album_size_of_match_music_files = 0
+        album_size_of_total_music_files = 0
+        album_size_of_match_other_files = 0
+        album_size_of_total_other_files = 0
+
+        print bcolors.cyan + "[%s]" % self.link_path + bcolors.none
+        album_number_of_album_directories += 1
+        album_number_of_directories_to_mk += 1
+
+        self.link_dirs.append(self.link_path)
+
+        for orig_dir in self.orig_dirs:
+            self.link_dirs.append(orig_dir.replace(ORIG_ROOT, LINK_ROOT, 1))
+            print bcolors.cyan + "[%s]" % orig_dir.replace(ORIG_ROOT, LINK_ROOT, 1) + bcolors.none
+            album_number_of_directories_to_mk += 1
+
+        for orig_track in self.orig_files:
+            if is_song(orig_track.filename):
+                album_number_of_total_music_files += 1
+                album_size_of_total_music_files += orig_track.size
+            else:
+                album_number_of_total_other_files += 1
+                album_size_of_total_other_files += orig_track.size
+
+            for perm_track in self.perm_files:
+                if is_song(orig_track.filename):
+                    if perm_track.md5 == orig_track.md5 and perm_track.possible_track_positions[0] in orig_track.possible_track_positions.values():
+#                       self.link_files[orig_track.linkfile] = perm_track.fullpath
+                        orig_track.match = perm_track.fullpath
+                        album_number_of_match_music_files += 1
+                        album_size_of_match_music_files += perm_track.size
+                else:
+                    if perm_track.md5 == orig_track.md5:
+#                       self.link_files[orig_track.linkfile] = perm_track.fullpath
+                        orig_track.match = perm_track.fullpath
+                        album_number_of_match_other_files += 1
+                        album_size_of_match_other_files += perm_track.size
+
+#           if not orig_track.linkfile in self.link_files:
+#               self.link_files[orig_track.linkfile] = orig_track.fullpath
+
+
+#           if self.link_files[orig_track.linkfile].startswith(PERM_ROOT):
+            if orig_track.match:
+                print bcolors.green + "%s" % orig_track.match + bcolors.blue + " -> %s" % orig_track.linkfile + bcolors.none
+#               print bcolors.green + "%s" % self.link_files[orig_track.linkfile] + bcolors.blue + " -> %s" % orig_track.linkfile + bcolors.none
+            else:
+                print bcolors.red + "%s" % orig_track.fullpath + bcolors.blue + " -> %s" % orig_track.linkfile + bcolors.none
+#               print bcolors.red + "%s" % self.link_files[orig_track.linkfile] + bcolors.blue + " -> %s" % orig_track.linkfile + bcolors.none
+
+        total_number_of_album_directories += album_number_of_album_directories
+        total_number_of_directories_to_mk += album_number_of_directories_to_mk
+        total_number_of_match_music_files += album_number_of_match_music_files
+        total_number_of_total_music_files += album_number_of_total_music_files
+        total_number_of_match_other_files += album_number_of_match_other_files
+        total_number_of_total_other_files += album_number_of_total_other_files
+        total_size_of_match_music_files += album_size_of_match_music_files
+        total_size_of_total_music_files += album_size_of_total_music_files
+        total_size_of_match_other_files += album_size_of_match_other_files
+        total_size_of_total_other_files += album_size_of_total_other_files
+
+        print "ALBUM: [ dirs: %d/%d ] [ music: %d/%d (%s/%s:%d%%) ] [ other: %d/%d (%s/%s:%d%%) ]" % (album_number_of_album_directories, album_number_of_directories_to_mk, album_number_of_match_music_files, album_number_of_total_music_files, locale.format("%d", album_size_of_match_music_files, grouping=True), locale.format("%d", album_size_of_total_music_files, grouping=True), (100*album_size_of_match_music_files/album_size_of_total_music_files), album_number_of_match_other_files, album_number_of_total_other_files, locale.format("%d", album_size_of_match_other_files, grouping=True), locale.format("%d", album_size_of_total_other_files, grouping=True), (100*album_size_of_match_other_files/album_size_of_total_other_files))
+
+        print "TOTAL: [ dirs: %d/%d ] [ music: %d/%d (%s/%s:%d%%) ] [ other: %d/%d (%s/%s:%d%%) ]" % (total_number_of_album_directories, total_number_of_directories_to_mk, total_number_of_match_music_files, total_number_of_total_music_files, binary_bytes(total_size_of_match_music_files), binary_bytes(total_size_of_total_music_files), (100*total_size_of_match_music_files/total_size_of_total_music_files), total_number_of_match_other_files, total_number_of_total_other_files, binary_bytes(total_size_of_match_other_files), binary_bytes(total_size_of_total_other_files), (100*total_size_of_match_other_files/total_size_of_total_other_files))
+        return True
+
+    def link_track(self):
+        for link_dir in self.link_dirs:
+            if not os.path.isdir(link_dir):
+                os.makedirs(link_dir)
+        for orig_file in self.orig_files:
+            if not os.path.isfile(orig_file.linkfile):
+                if orig_file.match:
+                    os.link(orig_file.match, orig_file.linkfile)
+                else:
+                    os.link(orig_file.fullpath, orig_file.linkfile)
+        return True
+
+    def diff_track(self):
+        sig_procs = []
+        delta_procs = []
+        def sig(match, sig):
+            sig_procs.append(subprocess.Popen(['rdiff','signature', match, sig], shell=False))
+        def delta(sig, orig, rdiff):
+            delta_procs.append(subprocess.Popen(['rdiff','delta', sig, orig, rdiff], shell=False))
+
+        matching_music_tracks = []
+        for orig_file in self.orig_files:
+            if orig_file.match and is_song(orig_file.filename):
+                matching_music_tracks.append(orig_file)
+
+        for track in matching_music_tracks:
+            sig(track.match, track.linkfile + ".sig")
+        for sig_proc in sig_procs:
+            sig_proc.wait()
+        for track in matching_music_tracks:
+            delta(track.linkfile + ".sig", track.fullpath, track.linkfile + ".rdiff")
+        for delta_proc in delta_procs:
+            delta_proc.wait()
+        for track in matching_music_tracks:
+            if os.path.isfile(track.linkfile + ".sig"):
+                os.remove(track.linkfile + ".sig")
+        print "finished diffing album: %s" % self.album
+        return True
+
+class Track:
+    fullpath = ""
+    filename = ""
+    size = ""
+    md5 = ""
+    linkfile = ""
+    match = ""
+
+    def __init__(self, fullpath, filename):
+        self.possible_track_positions = dict()
+
+        self.fullpath = fullpath
+        self.linkfile = self.fullpath.replace(ORIG_ROOT.encode('utf-8'), LINK_ROOT.encode('utf-8'), 1)
+        self.filename = filename
+        self.size = os.path.getsize(fullpath)
+        if is_song(filename):
+            for tpos in re.finditer(r'(?=(\d{2}))', filename):
+                self.possible_track_positions[tpos.start()]=tpos.group(1)
+#               for tpos in re.finditer(r'(?=(\d{2})[^\w])', filename):
+#                   self.possible_track_positions.append(tpos)
+                # use .start(), .start()+1 and .group(1)
+            self.md5 = md5song(fullpath)
+        else:
+            self.md5 = md5(fullpath)
+
+    def __cmp__(self, b):
+        if self.fullpath == b.fullpath: return 0
+        if self.fullpath < b.fullpath: return -1
+        if self.fullpath > b.fullpath: return 1
+
+    def __eq__(self, b):
+        return self.md5 == b.md5
+
+    def __str__(self):
+        return self.fullpath
+
+class bcolors:
+    red = '\033[31m'
+    green = '\033[32m'
+    yellow = '\033[33m'
+    blue = '\033[34m'
+    cyan = '\033[36m'
+    none = '\033[0m'
+
+def md5song(file):
+    f = open(file, 'rb')
+    f.seek(26)
+    return f.read(16).encode('hex_codec')
+
+def md5(file):
+    f = open(file, 'rb')
+    m = hashlib.md5()
+    while True:
+        data = f.read(8192)
+        if not data:
+            break
+        m.update(data)
+    return m.hexdigest()
+
+def is_song(filename):
+    return filename.lower().endswith('.flac')
+
+def find_albums():
+    albums = []
+    for tracker in os.listdir(ORIG_ROOT):
+        r = j(ORIG_ROOT, tracker)
+        if os.path.isdir(r) and tracker != "torrents":
+#           for album in glob(r + '/*'):
+            for album in os.listdir(r):
+                albums.append(Album(album, tracker))
+    return albums
+
+def walk_albums(albums):
+    for album in albums:
+        album.walk_album()
+    return True
+
+def pick_tracks(albums):
+    for album in albums:
+        album.pick_track()
+    return True
+
+def link_tracks(albums):
+    for album in albums:
+        album.link_track()
+    return True
+
+def diff_tracks(albums):
+    for album in albums:
+        album.diff_track()
+    return True
+
+def main():
+    albums = find_albums()
+    print "number of albums found in path: %d" % len(albums)
+    albums[:] = [album for album in albums if album.need_link]
+    albums.sort()
+    print "number of matches with library: %d" % len(albums)
+
+    walk_albums(albums)
+    pick_tracks(albums)
+    print "\nLinking files in 10 seconds. Press Ctrl-C to abort."
+    for wtf in range(10, -1, -1):
+        if wtf < 4:
+            print wtf
+        time.sleep(1)
+
+    link_tracks(albums)
+    diff_tracks(albums)
+
+if __name__ == "__main__":
+    sys.exit(main())
